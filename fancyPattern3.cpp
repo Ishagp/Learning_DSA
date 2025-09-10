f5674722b979e92f78770ddc55e7422eeacf3797
Ishagp <gisha825@gmail.com>
2025-09-10 17:02:30 +0530

    updated fancyPattern3

--- fancyPattern3.cpp
+++ fancyPattern3.cpp
@@ -5,23 +5,20 @@ int main()
     int n;
     cin >> n;
     int c = 1;
-    // Growing phase
     for (int i = 0; i < n; i++)
     {
-        int cond = i <= n/2 ? 2*i : 2*(n-i-1);
-        for (int j = 0; j <= cond ; j++)
+        int cond = i <= n / 2 ? 2 * i : 2 * (n - i - 1);
+        for (int j = 0; j <= cond; j++)
         {
-            if (j<=cond/2)
+            if (j <= cond / 2)
             {
-                cout<<j+1;
+                cout << j + 1;
             }
-            else{
-                cout<<cond-j+1;
+            else
+            {
+                cout << cond - j + 1;
             }
-            
         }
         cout << endl;
     }
-
-    
 }
