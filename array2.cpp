8c6b588bff2fd772b90cbd6d3e1b69196c8aecf1
Ishagp <gisha825@gmail.com>
2025-09-24 22:14:27 +0530

    .

--- array2.cpp
+++ array2.cpp
@@ -3,16 +3,16 @@ using namespace std;
 
 int main()
 {
-    int arr[10]; //declaration of array
+    int arr[3]; //declaration of array
     cout << "Enter the numbers in array" << endl;
-    for (int i = 0; i < 10; i++)
+    for (int i = 0; i < 3; i++)
     {
         cin >> arr[i]; // taking array elements from user
     }
     cout << "values are:" << endl;
-    for (int i = 0; i < 10; i++)
+    for (int i = 0; i < 3; i++)
     {
-        cout << 2*arr[i] << " ";  //printing array elements
+        cout << 6*arr[i] << " ";  //printing array elements
     }
     return 0;
 }
