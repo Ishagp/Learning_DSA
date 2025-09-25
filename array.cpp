ee15da75662dec3df43ebed02283be1223a365bf
Ishagp <gisha825@gmail.com>
2025-09-25 22:04:59 +0530

    arrayDeclaration

--- array.cpp
+++ array.cpp
@@ -3,14 +3,14 @@ using namespace std;
 
 int main()
 {
-    int arr[10]; //declaration of array
+    int arr[5]; //declaration of array
     cout << "Enter the numbers in array" << endl;
-    for (int i = 0; i < 10; i++)
+    for (int i = 0; i < 5; i++)
     {
         cin >> arr[i]; // taking array elements from user
     }
     cout << "values are:" << endl;
-    for (int i = 0; i < 10; i++)
+    for (int i = 0; i < 5; i++)
     {
         cout << arr[i] << " ";  //printing array elements
     }
