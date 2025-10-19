5736c5b4ad72c1de604397ea19b328be811e895e
Ishagp <gisha825@gmail.com>
2025-10-19 22:35:41 +0530

    new commit

---
+++ intersection.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main(){
+    vector<int> arr{1,5,8,7};
+    vector<int> brr{7,8,4};
+
+    vector<int> ans;
+
+    
+
+    return 0;
+}
