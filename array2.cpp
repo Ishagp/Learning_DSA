77119e83f6758b0d1c3fa9d579cc5a341a14dc6d
Ishagp <gisha825@gmail.com>
2025-12-17 11:14:21 +0530

    2nd largest and smallest in an array

---
+++ array2.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <vector>
+#include <climits>
+using namespace std;
+
+int secondLargest(vector<int> &a, int n)
+{
+    int largest = a[0];
+    int slargest = -1;
+    for (int i = 1; i < n; i++)
+    {
+        if (a[i] > largest)
+        {
+            slargest = largest;
+            largest = a[i];
+        }
+        else if (a[i] < largest && a[i] > slargest)
+        {
+            slargest = a[i];
+        }
+    }
+
+    return slargest;
+}
+
+int secondSmallest(vector<int> &a, int n)
+{
+    int smallest = a[0];
+    int ssmallest = INT_MAX;
+    for (int i = 1; i < n; i++)
+    {
+        if (a[i] < smallest)
+        {
+            ssmallest = smallest;
+            smallest = a[i];
+        }
+        else if (a[i] != smallest && a[i] < ssmallest)
+        {
+            ssmallest = a[i];
+        }
+    }
+
+    return ssmallest;
+}
+
+vector<int> getSecondOrderElemnts(int n, vector<int> a)
+{
+    int slargest = secondLargest(a, n);
+    int ssmallest = secondSmallest(a, n);
+    return {slargest, ssmallest};
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    cout << "Enter elements in array: ";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    vector<int> result = getSecondOrderElemnts(n, a);
+    
+    cout << "\nSecond Largest Element: " << result[0] << endl;
+    cout << "Second Smallest Element: " << result[1] << endl;
+    return 0;
+}
