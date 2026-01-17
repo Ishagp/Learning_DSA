c4ffd1e5da1b32f4540acda0e49c194c43e0c072
Ishagp <gisha825@gmail.com>
2026-01-17 23:23:53 +0530

    Missing num, max consecutive ones and single number

---
+++ array7.cpp
@@ -0,0 +1,55 @@
+// Missing number 
+
+#include <iostream>
+#include <vector>
+#include <set>
+using namespace std;
+
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int xor1=0;
+        int xor2=0;
+        int N = nums.size();
+        int n = N-1;
+        for(int i = 0; i <= n; i++){
+            xor2 = xor2 ^ nums[i];
+            xor1 = xor1 ^ (i+1);
+        }
+        // xor1 = xor1 ^ N;
+        return xor1 ^ xor2;
+    }
+};
+
+
+// Maximum consecutive ones
+class Solution {
+public:
+    int findMaxConsecutiveOnes(vector<int>& nums) {
+        int count = 0;
+        int maxi = 0;
+        for(int i = 0; i < nums.size(); i++){
+            if(nums[i] == 1){
+                count++;
+                maxi = max(maxi, count);
+            }
+            else{
+                count = 0;
+            }
+        }
+        return maxi;
+    }
+};
+
+
+// Single number(find the num that appear once)
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int xor1 = 0;
+        for(int i = 0; i < nums.size(); i++){
+            xor1 = xor1 ^ nums[i];
+        }
+        return xor1;
+    }
+};
